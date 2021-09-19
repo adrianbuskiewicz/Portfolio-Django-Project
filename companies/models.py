2b4acd9eb0f8d3ab44f78fcfe564da798ea90eaf
Adrian Buśkiewicz <adrian.k.buskiewicz@gmail.com>
2021-09-19 23:06:05 +0200

    Company model created

--- companies/models.py
+++ companies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Company(models.Model):
+    symbol = models.CharField(max_length=5)
+    name = models.CharField(max_length=100)
+    rate_of_return = models.FloatField(null=True, blank=True)
+    volatility = models.FloatField(null=True, blank=True)
+    user = models.ManyToManyField(User)
+
+    def __str__(self):
+        return str(self.name)
+

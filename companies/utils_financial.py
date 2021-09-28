c86b490b53e3b7bc4a162690c7a71f519e34ce8e
Adrian Buśkiewicz <adrian.k.buskiewicz@gmail.com>
2021-09-28 04:16:40 +0200

    Updated reader model
    Changed pandas data reader for yfinance (works much quicker)

--- companies/utils_financial.py
+++ companies/utils_financial.py
@@ -1,19 +1,30 @@
-import pandas_datareader.data as web
-import pandas as pd
+import yfinance as yf
+import numpy as np
 
 
-def company_df(symbols=None):
-    df = web.DataReader(symbols, 'yahoo')
+def company_df(symbols=None, date_from=None, date_to=None):
+    df = yf.download(
+        symbols,
+        start=date_from,
+        end=date_to,
+    )
     df = df['Adj Close']
-    df.fillna('mean')
+    df = np.log(df/df.shift(1))*100
     return df
 
 
-def df_mean_price(symbol):
-    df = company_df(symbol)
-    return df.mean()
+def df_mean_price(symbol, date_from=None, date_to=None):
+    df = company_df(symbol, date_from=date_from, date_to=date_to)
+    return round(df.mean(), 4)
 
 
-def df_vol_price(symbol):
-    df = company_df(symbol)
-    return df.std()
+def df_vol_price(symbol, date_from=None, date_to=None):
+    df = company_df(symbol, date_from=date_from, date_to=date_to)
+    return round(df.std(), 4)
+
+
+def corr_matrix(rates_df, style):
+    corr_mat = rates_df.corr()
+    corr_mat = corr_mat.to_html()
+    corr_mat = corr_mat.replace('class="dataframe"', style)
+    return corr_mat

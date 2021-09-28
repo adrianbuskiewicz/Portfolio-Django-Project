35fb4320146e65cfdef93974a7615876e5c920eb
Adrian Buśkiewicz <adrian.k.buskiewicz@gmail.com>
2021-09-28 04:15:15 +0200

    Registered admin views

--- portfolios/admin.py
+++ portfolios/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import Portfolio, CompanyWeight
+from .models import (
+    Portfolio,
+    CompanyWeight,
+    MVP,
+    ORP,
+    BoughtPortfolio,
+)
 
 # Register your models here.
 
 admin.site.register(Portfolio)
 admin.site.register(CompanyWeight)
+admin.site.register(BoughtPortfolio)
+admin.site.register(MVP)
+admin.site.register(ORP)

5011bc6d9d265b8a58470e8c24df301254fec423
Adrian Buśkiewicz <adrian.k.buskiewicz@gmail.com>
2021-09-28 04:20:01 +0200

    Created forms for companies
    Forms for creating company (CompanyForm) and
    for calculating companies financial profiles (CalculateStatsForm)

--- companies/forms.py
+++ companies/forms.py
@@ -1,8 +1,21 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Company
+from datetime import date, timedelta
+
+TODAY = date.today()
+YEAR_AGO = date.today()-timedelta(days=365)
 
 
-class CompanyForm(ModelForm):
+class CompanyForm(forms.ModelForm):
     class Meta:
         model = Company
         fields = ['symbol', 'name']
+
+
+class CalculateStatsForm(forms.Form):
+    date_from = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}),
+                                initial=YEAR_AGO)
+    date_to = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}),
+                              initial=TODAY)
+
+

de848697831f8b94f9380e76bc91d550031540e4
Adrian Buśkiewicz <adrian.k.buskiewicz@gmail.com>
2021-10-04 23:39:18 +0200

    Update urls.py
    New urls added for bought portfolios actions

--- portfolios/urls.py
+++ portfolios/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
-from .views import calculate_portfolios_view
+from .views import (
+    calculate_portfolios_view,
+    not_use_company_view,
+    BoughtPortfolios,
+    BoughtPortfolioDetail,
+    create_bought_portfolio_view,
+)
+
 
 app_name = 'portfolios'
 
 urlpatterns = [
     path('portfolios/', calculate_portfolios_view, name='create_portfolios'),
+    path('portfolios/not-use-company/<pk>/', not_use_company_view, name='not_use_company'),
+    path('portfolios/bought-portfolios/', BoughtPortfolios.as_view(), name='bought_portfolios'),
+    path('portfolios/bought-portfolios/<pk>/', BoughtPortfolioDetail.as_view(), name='bought_portfolio_info'),
+    path('portfolios/create-bought-portfolio/<pk>', create_bought_portfolio_view, name='create_bought_portfolio'),
 ]

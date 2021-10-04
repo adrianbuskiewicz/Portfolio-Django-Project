64a3c0bafdade978f35d6f4807f607ed9c0cf9d4
Adrian Buśkiewicz <adrian.k.buskiewicz@gmail.com>
2021-10-04 23:39:48 +0200

    Update forms.py
    New forms for Calculating Portfolio and Investing in Portfolio

--- portfolios/forms.py
+++ portfolios/forms.py
@@ -1,5 +1,12 @@
 from django import forms
+from .models import BoughtPortfolio
 
 
 class CalculatePortfoliosForm(forms.Form):
     rf = forms.FloatField(initial=1)
+
+
+class BoughtPortfolioForm(forms.ModelForm):
+    class Meta:
+        model = BoughtPortfolio
+        fields = ['budget']

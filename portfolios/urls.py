e4d37a5c97564f125f798571c32c7af3469151aa
Adrian Buśkiewicz <adrian.k.buskiewicz@gmail.com>
2021-10-08 16:47:13 +0200

    Renamed company views

--- portfolios/urls.py
+++ portfolios/urls.py
@@ -5,15 +5,17 @@ from .views import (
     BoughtPortfolios,
     BoughtPortfolioDetail,
     create_bought_portfolio_view,
+    delete_bought_portfolio_view,
 )
 
 
 app_name = 'portfolios'
 
 urlpatterns = [
-    path('portfolios/', calculate_portfolios_view, name='create_portfolios'),
-    path('portfolios/not-use-company/<pk>/', not_use_company_view, name='not_use_company'),
-    path('portfolios/bought-portfolios/', BoughtPortfolios.as_view(), name='bought_portfolios'),
-    path('portfolios/bought-portfolios/<pk>/', BoughtPortfolioDetail.as_view(), name='bought_portfolio_info'),
-    path('portfolios/create-bought-portfolio/<pk>', create_bought_portfolio_view, name='create_bought_portfolio'),
+    path('', calculate_portfolios_view, name='create_portfolios'),
+    path('not-use-company/<pk>/', not_use_company_view, name='not_use_company'),
+    path('bought-portfolios/', BoughtPortfolios.as_view(), name='bought_portfolios'),
+    path('bought-portfolios/<pk>/', BoughtPortfolioDetail.as_view(), name='bought_portfolio_info'),
+    path('bought-portfolios/create-bought-portfolio/<pk>/', create_bought_portfolio_view, name='create_bought_portfolio'),
+    path('bought-portfolios/delete/<pk>/', delete_bought_portfolio_view, name='delete_bought_portfolio'),
 ]
